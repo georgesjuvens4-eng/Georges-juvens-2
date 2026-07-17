81b9b6cc43c425553c62d528000c46857675ee87
georgesjuvens4-eng <georgesjuvens4@gmail.com>
2026-07-17 13:23:06 -0400

    c.py

---
+++ c.py
@@ -0,0 +1,13 @@
+
+# a voir les structure de fichier dans le fichier readme 
+pour afin de les cree pour que le program se lance 
+
+# se fichier se trouve dans le docier use_case
+
+from interface_adapter.b import B 
+
+class C :
+    def get2(self):
+        b = B()
+        c = str (b.get1())
+        return c 
