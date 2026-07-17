53a06899e7f63b3041aed726589a2e671ff5940c
georgesjuvens4-eng <georgesjuvens4@gmail.com>
2026-07-17 13:34:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# a voir les structure de fichier dans le fichier readme 
+pour afin de les cree pour que le program se lance 
+
+# se fichier se trouve dans le docier source qui s appel
+project 
+
+
+from entiter.d import D 
+
+d = D()
+d.execut()
