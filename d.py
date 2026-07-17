dfaa83bbba4b3c6c38c38260efd1ecf9380865c6
georgesjuvens4-eng <georgesjuvens4@gmail.com>
2026-07-17 13:09:20 -0400

    Create d.py

---
+++ d.py
@@ -0,0 +1,13 @@
+# a voir le fichier readme afin de voir la structure de fichier 
+pour pouvoir l abcer le programme 
+
+
+
+from use_case.c import C
+import webbrowser 
+
+class D :
+    def execut(self):
+        c = C()
+        message = str (c.get2() )
+        webbrowser.open(message)
