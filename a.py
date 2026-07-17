d4299d06e57d507e67fca4243bcde909e1db7188
georgesjuvens4-eng <georgesjuvens4@gmail.com>
2026-07-17 13:31:15 -0400

    Create a.py

---
+++ a.py
@@ -0,0 +1,17 @@
+# a voir les structure de fichier dans le fichier readme 
+pour afin de les cree pour que le program se lance 
+
+# se fichier se trouve dans le docier framworks 
+
+
+
+print ( " bienvenu sur web shear on Terminal or WST")
+
+class A : 
+    
+    def recevoir (self) :
+        url =  str (input (" Entre un url valide avec le sufixe .com : ") )
+        return url
+
+
+
